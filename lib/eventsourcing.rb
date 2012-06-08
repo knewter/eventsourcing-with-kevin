741f27fc4257bc1ff4c5ed88729c8932d09a0fc8
Kevin Russell <russellkt@gmail.com>
2012-06-08 14:33:02 -0500

    adding event interface

--- lib/eventsourcing.rb
+++ lib/eventsourcing.rb
@@ -1,6 +1,7 @@
 # This is the main lib file.
 require_relative './eventsourcing/event_store'
 require_relative './eventsourcing/event_stores/array'
+require_relative './eventsourcing/event'
 
 # Initialize an event store so other things can use it
 event_store = EventStore.new
