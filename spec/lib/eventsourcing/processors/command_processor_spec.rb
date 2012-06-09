72a698e4e5d0aa65903b7b6e183f1572bac8c556
Josh Adams <josh@isotope11.com>
2012-06-09 10:24:57 -0500

    Allow event handlers to be added to a CommandProcessor

--- spec/lib/eventsourcing/processors/command_processor_spec.rb
+++ spec/lib/eventsourcing/processors/command_processor_spec.rb
@@ -4,4 +4,10 @@ describe CommandProcessor do
   before do
     @processor = CommandProcessor.new
   end
+
+  it "can add handlers" do
+    handler = MiniTest::Mock.new
+    @processor.add_handler('some_event_type', handler)
+    @processor.send(:handler_for, 'some_event_type').must_equal handler
+  end
 end
